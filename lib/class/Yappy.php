ab045012da0bd83003c9d27365801e87adae2182
Angel Morales <angelbrayan25@gmail.com>
2023-12-26 20:51:49 -0500

    fix transactionType

--- lib/class/Yappy.php
+++ lib/class/Yappy.php
@@ -7,7 +7,7 @@
         private static $secretKey   = 'WVBfRTAwOUYwNzItMzBCMi0zRDg3LTgyMjktMzJGNDQ0Njk3RjVCLmM2NzEzNTJkLWI4YmEtNDBmNy05Y2FhLTEyM2RlNzVhN2Q0NQ==';
         private static $url_success = 'https://customers.ylcboxespanama.com/';
         private static $url_fail    = 'https://customers.ylcboxespanama.com/';
-        private static $domain        = 'https://ylcboxespanama.com/';
+        private static $domain      = 'https://customers.ylcboxespanama.com/';
         public static function  sendPayment($data){
             $checkCredentials = self::checkCredentials(self::$merchantId, self:: $secretKey, self::$domain );
             if($checkCredentials['success'] = true){
